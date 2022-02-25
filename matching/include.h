gfiumara/MSU-LatentAFIS

commit b2775eb09b27260378d350aef35861584a505774

No more pointless constructors or unique_ptr

matching/include.h
@@ -38,110 +38,59 @@ enum FPType {Latent,Rolled};
 class SingleTemplate
 {
     public:
-        int m_nrof_minu;
-        int m_nrof_feature;
-        int m_des_length;
-        int m_block_size;
-        int m_blkH;
-        int m_blkW;
+        int m_nrof_feature{0};
+        int m_des_length{0};
+        int m_block_size{16};
+        int m_blkH{0};
+        int m_blkW{0};
         std::vector<float> m_des{};
         std::vector<MinuPoint> m_minutiae{};
-        TemplateType m_template_type;
-        SingleTemplate()
+        TemplateType m_template_type{TemplateType::Minutiae};
+        SingleTemplate() = default;
+
+	SingleTemplate(const TemplateType type) : m_template_type{type}{};
+
+        SingleTemplate(const int nrof_minutiae, const int des_length):
+            m_nrof_minu(nrof_minutiae),
+            m_des_length(des_length),
+            m_des(m_nrof_minu*m_des_length),
+            m_minutiae(m_nrof_minu)
         {
-            m_nrof_minu = 0;
-            m_nrof_feature = 0;
-            m_des_length = 0;
-            m_template_type = TemplateType::Minutiae;
-            m_block_size = 16;
-            m_blkH = 0;
-            m_blkW = 0;
-        };
-        SingleTemplate(const int nrof_minutiae, const int des_length):m_nrof_minu(nrof_minutiae),m_des_length(des_length)
-        {
-            m_des = std::vector<float>(m_nrof_minu*m_des_length);
-            m_minutiae = std::vector<MinuPoint>(m_nrof_minu);
-            m_block_size = 16;
         };
 
-
-
-        void initialization(const int nrof_minutiae, const int des_length)
+        SingleTemplate(const int nrof_minutiae, const int des_length, const std::vector<float> des, const int blkH, const int blkW):
+        m_nrof_minu{nrof_minutiae},
+        m_des_length{des_length},
+        m_blkH{blkH},
+        m_blkW{blkW},
+        m_des{des},
+        m_minutiae(nrof_minutiae)
         {
-            m_nrof_minu = nrof_minutiae;
-            m_des_length = des_length;
-            m_des = std::vector<float>(m_nrof_minu*m_des_length);
-            m_minutiae = std::vector<MinuPoint>(m_nrof_minu);
-            m_block_size = 16;
-        };
-
-        SingleTemplate(const SingleTemplate &temp)
-        {
-            m_nrof_minu = temp.m_nrof_minu;
-            m_des_length = temp.m_des_length;
-            //copy minutiae descriptor
-            m_des = temp.m_des;
-            m_minutiae = std::vector<MinuPoint>(temp.m_minutiae);
-            m_block_size = 16;
-
-            m_template_type = temp.m_template_type;
-            m_blkH = 0;
-            m_blkW = 0;
-            if(m_template_type==TemplateType::Minutiae)
-            {
-                m_blkH = temp.m_blkH;
-                m_blkW = temp.m_blkW;
-            }
-        };
-        SingleTemplate& operator=(const SingleTemplate &temp)
-        {
-            m_nrof_minu = temp.m_nrof_minu;
-            m_des_length = temp.m_des_length;
-            //copy minutiae descriptor
-            m_des = temp.m_des;
-            m_minutiae = std::vector<MinuPoint>(temp.m_minutiae);
-            m_block_size = 16;
-
-            m_template_type = temp.m_template_type;
-            m_blkH = 0;
-            m_blkW = 0;
-            if(m_template_type==TemplateType::Texture)
-            {
-                m_blkH = temp.m_blkH;
-                m_blkW = temp.m_blkW;
-            }
-
-            return (*this);
         }
-        ~ SingleTemplate()
+
+        SingleTemplate(const int nrof_minutiae, const int des_length, const std::vector<float> des):
+        m_nrof_minu{nrof_minutiae},
+        m_des_length{des_length},
+        m_des{des},
+        m_minutiae(nrof_minutiae)
         {
-            m_nrof_minu = 0;
-            m_nrof_feature = 0;
-            m_des_length = 0;
-            m_block_size = 0;
-        };
-        void release(void)
-        {
-            m_nrof_minu = 0;
-            m_nrof_feature = 0;
-            m_des_length = 0;
-            m_block_size = 0;
         }
-        void set_x(const short *x)
+
+        void set_x(const std::vector<short> &x)
         {
             for(int i=0;i<m_nrof_minu; ++i)
             {
                 m_minutiae[i].x = x[i];
             }
         }
-        void set_y(const short *y)
+        void set_y(const std::vector<short> &y)
         {
             for(int i=0;i<m_nrof_minu; ++i)
             {
                 m_minutiae[i].y = y[i];
             }
         };
-        void set_ori(const float *ori)
+        void set_ori(const std::vector<float> &ori)
         {
             for(int i=0;i<m_nrof_minu; ++i)
             {
@@ -159,122 +108,76 @@ class SingleTemplate
 
 class MinutiaeTemplate:public SingleTemplate{
     public:
-        MinutiaeTemplate()
+        MinutiaeTemplate() : SingleTemplate()
         {
-            m_template_type = TemplateType::Minutiae;
         };
+
         MinutiaeTemplate(const int nrof_minutiae, const int des_length):SingleTemplate(nrof_minutiae, des_length)
         {
-            m_template_type = TemplateType::Minutiae;
         };
+
         // minutiae template initialization
         // minutiae, minutiae descriptor and ridge flow are included in minutiae template
-        MinutiaeTemplate(const int nrof_minutiae, const short *x,const short *y,const float *ori,const int des_length, const float *des, const int blkH, const int blkW, const float *oimg):
-        SingleTemplate(nrof_minutiae, des_length)
+        MinutiaeTemplate(const int nrof_minutiae, const std::vector<short> &x,const std::vector<short> &y,const std::vector<float> &ori,const int des_length, const std::vector<float> &des, const int blkH, const int blkW):
+        SingleTemplate(nrof_minutiae, des_length, des, blkH, blkW)
         {
-            m_template_type = TemplateType::Minutiae;
-
-            SingleTemplate::m_blkH = blkH;
-            SingleTemplate::m_blkW = blkW;
-
-            // minutiae descriptor
-            m_des = std::vector<float>(m_nrof_minu*m_des_length);
-            std::memcpy (m_des.data(), des, sizeof(float)*m_nrof_minu*m_des_length);
-
             // minutiae
             set_x(x);
             set_y(y);
             set_ori(ori);
         };
-        void initialization(const int nrof_minutiae, const int des_length)
-        {
-            // release();
-            m_nrof_minu = nrof_minutiae;
-            m_des_length = des_length;
-            m_des = std::vector<float>(m_nrof_minu*m_des_length);
-            m_minutiae = std::vector<MinuPoint>(m_nrof_minu);
-            m_block_size = 16;
-        };
 
-        ~ MinutiaeTemplate()
-        {
-
-        };
 };
 
 class TextureTemplate: public SingleTemplate
 {
     public:
-        TextureTemplate()
+        TextureTemplate() : SingleTemplate(TemplateType::Texture)
         {
-            m_template_type = TemplateType::Texture;
         };
         TextureTemplate(const int nrof_minutiae, const int des_length):SingleTemplate(nrof_minutiae, des_length)
         {
-            m_template_type = TemplateType::Texture;
+        	this->set_type(TemplateType::Texture);
         };
         // texture template initialization
         // Only minutiae and minutiae descriptors are included in texture template
-        TextureTemplate(const int nrof_minutiae, const short *x,const short *y,const float *ori, const int des_length, const float *des):SingleTemplate(nrof_minutiae, des_length)
+        TextureTemplate(const int nrof_minutiae, const std::vector<short> &x,const std::vector<short> &y,const std::vector<float> &ori, const int des_length, const std::vector<float> &des):
+        SingleTemplate(nrof_minutiae, des_length, des)
         {
-            //release();
-            m_template_type = TemplateType::Texture;
+            this->set_type(TemplateType::Texture);
 
-            if(des){
-                std::memcpy(m_des.data(), des, sizeof(float)*m_nrof_minu*m_des_length);
-            }
             set_x(x);
             set_y(y);
             set_ori(ori);
-
-            m_blkH = 0;
-            m_blkW = 0;
         };
+
         void initialization(const int nrof_minutiae, const int des_length)
         {
             m_nrof_minu = nrof_minutiae;
             m_des_length = des_length;
             m_des = std::vector<float>(m_nrof_minu*m_des_length);
+
             m_minutiae = std::vector<MinuPoint>(m_nrof_minu);
             m_block_size = 16;
-        };
-
-        ~ TextureTemplate()
-        {
-
         };
 };
 
 class LatentTextureTemplate: public TextureTemplate
 {
     public:
-        std::vector<float> m_dist_codewords;
-        LatentTextureTemplate()
+        std::vector<float> m_dist_codewords{};
+        LatentTextureTemplate() : TextureTemplate()
         {
-            m_dist_codewords.clear();
         };
+
         LatentTextureTemplate(const int nrof_minutiae, const int des_length):TextureTemplate(nrof_minutiae,des_length)
         {
-
-            m_des = std::vector<float>(m_nrof_minu*m_des_length);
-            m_minutiae = std::vector<MinuPoint>(m_nrof_minu);
-            m_block_size = 16;
         };
-        LatentTextureTemplate(const int nrof_minutiae, const short *x,const short *y,const float *ori, const int des_length, const float *des):
+        LatentTextureTemplate(const int nrof_minutiae, const std::vector<short> &x,const std::vector<short> &y,const std::vector<float> &ori, const int des_length, const std::vector<float> &des):
         TextureTemplate(nrof_minutiae, x, y, ori, des_length, des)
-        {
-        };
-        void initialization(const int nrof_minutiae, const int des_length, const int nrof_subs,  const int nrof_clusters)
-        {
-            m_nrof_minu = nrof_minutiae;
-            m_des_length = des_length;
-            m_des = std::vector<float>(m_nrof_minu*m_des_length);
+        {}
 
-            m_minutiae = std::vector<MinuPoint>(m_nrof_minu);
-            m_block_size = 16;
-        };
-
-         void compute_dist_to_codewords(const std::vector<float> &codewords, const int nrof_subs, const int sub_dim,  const int nrof_clusters)
+        void compute_dist_to_codewords(const std::vector<float> &codewords, const int nrof_subs, const int sub_dim,  const int nrof_clusters)
         {
             m_dist_codewords.resize(m_nrof_minu*nrof_subs*nrof_clusters);
 
@@ -306,50 +209,13 @@ class LatentTextureTemplate: public TextureTemplate
 
 
         };
-
-        ~LatentTextureTemplate()
-        {
-        };
 };
 
 class RolledTextureTemplatePQ:public TextureTemplate
 {
     public:
-        std::vector<unsigned char> m_desPQ;
-        RolledTextureTemplatePQ()
-        {
-            m_nrof_minu = 0;
-            m_nrof_feature = 0;
-            m_des_length = 0;
-            m_block_size = 16;
-        };
-        RolledTextureTemplatePQ(const int nrof_minutiae, const int des_length):TextureTemplate(nrof_minutiae, des_length)
-        {
-            m_desPQ = std::vector<unsigned char>(m_nrof_minu*m_des_length);
-        };
-
-        RolledTextureTemplatePQ(const RolledTextureTemplatePQ & input_template) :
-        TextureTemplate(input_template.m_nrof_minu, input_template.m_des_length)
-        {
-            m_nrof_minu = input_template.m_nrof_minu;
-            m_des_length = input_template.m_des_length;
-
-            m_block_size = input_template.m_block_size;
-            m_block_size = input_template.m_block_size;
-
-            m_desPQ = std::vector<unsigned char>(m_nrof_minu*m_des_length);
-            std::memcpy(m_desPQ.data(), input_template.m_desPQ.data(), sizeof(unsigned char)*m_nrof_minu*m_des_length);
-
-            m_minutiae = std::vector<MinuPoint>(input_template.m_minutiae);
-        };
-
-
-        RolledTextureTemplatePQ(const int nrof_minutiae, const short *x,const short *y,const float *ori, const int des_length, const float *des):
-        TextureTemplate(nrof_minutiae, x, y, ori, des_length, NULL)
-        {
-            m_desPQ = std::vector<unsigned char>(m_nrof_minu*m_des_length);
-            std::memcpy(m_desPQ.data(), des, sizeof(char)*m_nrof_minu*m_des_length);
-        };
+        std::vector<unsigned char> m_desPQ{};
+        RolledTextureTemplatePQ() : TextureTemplate() {}
         void initialization(const int nrof_minutiae, const int des_length)
         {
             // release();
@@ -358,57 +224,31 @@ class RolledTextureTemplatePQ:public TextureTemplate
             m_desPQ = std::vector<unsigned char>(m_nrof_minu*m_des_length);
             m_minutiae = std::vector<MinuPoint>(m_nrof_minu);
             m_block_size = 16;
-        };
-
-        ~RolledTextureTemplatePQ()
-        {
-            m_nrof_minu = 0;
-            m_nrof_feature = 0;
-            m_des_length = 0;
-            m_block_size = 0;
-        };
-        void release(void)
-        {
-            m_nrof_minu = 0;
-            m_nrof_feature = 0;
-            m_des_length = 0;
-            m_block_size = 0;
         }
-        void set_x(short *x)
+        RolledTextureTemplatePQ(const int nrof_minutiae, const int des_length):TextureTemplate(nrof_minutiae, des_length),
+            m_desPQ(m_nrof_minu*m_des_length)
         {
-            for(int i=0;i<m_nrof_minu; ++i)
-            {
-                m_minutiae[i].x = x[i];
-            }
         }
-        void set_y(short *y)
-        {
-            for(int i=0;i<m_nrof_minu; ++i)
-            {
-                m_minutiae[i].y = y[i];
-            }
-        };
-        void set_ori(float *ori)
-        {
-            for(int i=0;i<m_nrof_minu; ++i)
-            {
-                m_minutiae[i].ori = ori[i];
-            }
-        };
 
+        RolledTextureTemplatePQ(const int nrof_minutiae, const std::vector<short> &x,const std::vector<short> &y,const std::vector<float> &ori, const int des_length, const std::vector<float> &des):
+        TextureTemplate(nrof_minutiae, x, y, ori, des_length, {})
+        {
+        	/* FIXME: m_desPQ is unsigned char, but storing floats? Bug? */
+        	for (const float &f : des)
+        		m_desPQ.emplace_back(static_cast<unsigned char>(f));
+        }
 };
 
 class FPTemplate
 {
     public:
-        int m_nrof_minu_templates;
-        int m_nrof_texture_templates;
-        std::vector<MinutiaeTemplate> m_minu_templates;
+        int m_nrof_minu_templates{0};
+        int m_nrof_texture_templates{0};
+        std::vector<MinutiaeTemplate> m_minu_templates{};
         FPType m_FP_type;
-        FPTemplate(FPType FP_type):m_FP_type(FP_type)
+
+        FPTemplate(const FPType FP_type):m_FP_type(FP_type)
         {
-            m_nrof_minu_templates = 0;
-            m_nrof_texture_templates = 0;
         };
         void add_template(const MinutiaeTemplate & minutiae_template)
         {
@@ -417,10 +257,7 @@ class FPTemplate
             m_nrof_minu_templates++;
 
         };
-        ~FPTemplate()
-        {
-            release();
-        };
+
         void release()
         {
             m_minu_templates.clear();
@@ -431,7 +268,7 @@ class FPTemplate
 
 class LatentFPTemplate:public FPTemplate{
 public:
-    std::vector<LatentTextureTemplate> m_texture_templates;
+    std::vector<LatentTextureTemplate> m_texture_templates{};
     LatentFPTemplate():FPTemplate(Latent){};
     void release()
     {
@@ -451,7 +288,7 @@ public:
 
 class RolledFPTemplate:public FPTemplate{
 public:
-    std::vector<RolledTextureTemplatePQ> m_texture_templates;
+    std::vector<RolledTextureTemplatePQ> m_texture_templates{};
     RolledFPTemplate():FPTemplate(Rolled){};
     void release()
     {

matching/matcher.cpp
@@ -92,6 +92,7 @@ Matcher::Matcher(string code_file)
     }
 }
 
+#if 0
 int Matcher::List2List_matching(string latent_path, string rolled_path, string score_path)
 {
     string template_file, score_file;
@@ -334,7 +335,7 @@ int Matcher::One2List_matching(string latent_template_file_string, string rolled
     }
     return 0;
 }
-
+#endif
 int Matcher::One2One_matching_all_templates(const LatentFPTemplate &latent_template, const RolledFPTemplate &rolled_template, vector<float> & score)
 {
 
@@ -521,6 +522,7 @@ const
 
 }
 
+#if 0
 int Matcher::One2One_matching(string latent_file, string rolled_file)
 {
     LatentFPTemplate latent_template;
@@ -533,6 +535,7 @@ int Matcher::One2One_matching(string latent_file, string rolled_file)
 
     return ret;
 }
+#endif
 
 float Matcher::One2One_texture_matching(const LatentTextureTemplate &latent_texture_template, const RolledTextureTemplatePQ &rolled_texture_template)
 const
@@ -787,6 +790,7 @@ const
 
 }
 
+#if 0
 int Matcher::load_FP_template(string tname, LatentFPTemplate & fp_template)
 {
     fp_template.release();
@@ -887,6 +891,7 @@ int Matcher::load_FP_template(string tname, LatentFPTemplate & fp_template)
 
     return 0;
 }
+#endif
 
 void Matcher::load_FP_template(const std::vector<uint8_t> &buf, LatentFPTemplate & fp_template) const
 {
@@ -918,12 +923,10 @@ void Matcher::load_FP_template(const std::vector<uint8_t> &buf, LatentFPTemplate
     short des_len;
     int i,j;
 
-    std::unique_ptr<short[]> x{new short[Max_Nrof_Minutiae]};
-    std::unique_ptr<short[]> y{new short[Max_Nrof_Minutiae]};
-    std::unique_ptr<float[]> ori{new float[Max_Nrof_Minutiae]};
-    std::unique_ptr<float[]> oimg{new float[Max_BlkSize*Max_BlkSize]};
-
-    float des[Max_Nrof_Minutiae*Max_Des_Length];
+    std::vector<short> x(Max_Nrof_Minutiae);
+    std::vector<short> y(Max_Nrof_Minutiae);
+    std::vector<float> ori(Max_Nrof_Minutiae);
+    std::vector<float> des(Max_Nrof_Minutiae*Max_Des_Length);
 
     for(int i=0; i<12; i++){
         is.read(reinterpret_cast<char*>(&header[i]),sizeof(short));
@@ -966,14 +969,14 @@ void Matcher::load_FP_template(const std::vector<uint8_t> &buf, LatentFPTemplate
             throw std::runtime_error{"The size of the ridge flow is larger "
                 "than maximum size: " + std::to_string(Max_BlkSize)};
         }
-        is.read(reinterpret_cast<char*>(x.get()),sizeof(short)*nrof_minutiae);
-        is.read(reinterpret_cast<char*>(y.get()),sizeof(short)*nrof_minutiae);
-        is.read(reinterpret_cast<char*>(ori.get()),sizeof(float)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(x.data()),sizeof(short)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(y.data()),sizeof(short)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(ori.data()),sizeof(float)*nrof_minutiae);
         is.read(reinterpret_cast<char*>(&des_len),sizeof(short));
 
-        is.read(reinterpret_cast<char*>(des),sizeof(float)*nrof_minutiae*des_len);
+        is.read(reinterpret_cast<char*>(des.data()),sizeof(float)*nrof_minutiae*des_len);
 
-        MinutiaeTemplate minu_template(nrof_minutiae,x.get(),y.get(),ori.get(),des_len,des,blkH, blkW, oimg.get());
+        MinutiaeTemplate minu_template(nrof_minutiae,x,y,ori,des_len,des,blkH, blkW);
         fp_template.add_template(minu_template);
     }
 
@@ -990,19 +993,20 @@ void Matcher::load_FP_template(const std::vector<uint8_t> &buf, LatentFPTemplate
                 "Number of Minutiae: " + std::to_string(nrof_minutiae) + ">" +
                 std::to_string(Max_Nrof_Minutiae)};
         }
-        is.read(reinterpret_cast<char*>(x.get()),sizeof(short)*nrof_minutiae);
-        is.read(reinterpret_cast<char*>(y.get()),sizeof(short)*nrof_minutiae);
-        is.read(reinterpret_cast<char*>(ori.get()),sizeof(float)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(x.data()),sizeof(short)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(y.data()),sizeof(short)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(ori.data()),sizeof(float)*nrof_minutiae);
         is.read(reinterpret_cast<char*>(&des_len),sizeof(short));
-        is.read(reinterpret_cast<char*>(des),sizeof(float)*nrof_minutiae*des_len);
+        is.read(reinterpret_cast<char*>(des.data()),sizeof(float)*nrof_minutiae*des_len);
 
 
-        LatentTextureTemplate texture_template(nrof_minutiae,x.get(),y.get(),ori.get(),des_len,des);
+        LatentTextureTemplate texture_template(nrof_minutiae,x,y,ori,des_len,des);
         texture_template.compute_dist_to_codewords(this->codewords, nrof_subs,  sub_dim,  nrof_clusters);
         fp_template.add_texture_template(texture_template);
     }
 }
 
+#if 0
 int Matcher::load_FP_template(string tname, RolledFPTemplate & fp_template)
 {
     fp_template.release();
@@ -1101,6 +1105,7 @@ int Matcher::load_FP_template(string tname, RolledFPTemplate & fp_template)
 
     return 0;
 }
+#endif
 
 void Matcher::load_FP_template(const std::vector<uint8_t> &buf, RolledFPTemplate & fp_template) const
 {
@@ -1133,13 +1138,12 @@ void Matcher::load_FP_template(const std::vector<uint8_t> &buf, RolledFPTemplate
     short des_len=96;
     int i,j;
 
-    std::unique_ptr<short[]> x{new short[Max_Nrof_Minutiae]};
-    std::unique_ptr<short[]> y{new short[Max_Nrof_Minutiae]};
-    std::unique_ptr<float[]> ori{new float[Max_Nrof_Minutiae]};
-    std::unique_ptr<float[]> reliability{new float[Max_Nrof_Minutiae]};
-    std::unique_ptr<float[]> oimg{new float[Max_BlkSize*Max_BlkSize]};
+    std::vector<short> x(Max_Nrof_Minutiae);
+    std::vector<short> y(Max_Nrof_Minutiae);
+    std::vector<float> ori(Max_Nrof_Minutiae);
+    std::vector<float> reliability(Max_Nrof_Minutiae);
 
-    std::unique_ptr<float[]> des{new float[Max_Nrof_Minutiae*Max_Des_Length]};
+    std::vector<float> des(Max_Nrof_Minutiae*Max_Des_Length);
 
     for(int i=0; i<12; i++){
         is.read(reinterpret_cast<char*>(&header[i]),sizeof(short));
@@ -1181,14 +1185,14 @@ void Matcher::load_FP_template(const std::vector<uint8_t> &buf, RolledFPTemplate
             throw std::runtime_error{"The size of the ridge flow is larger "
                 "than maximum size: " + std::to_string(Max_BlkSize)};
         }
-        is.read(reinterpret_cast<char*>(x.get()),sizeof(short)*nrof_minutiae);
-        is.read(reinterpret_cast<char*>(y.get()),sizeof(short)*nrof_minutiae);
-        is.read(reinterpret_cast<char*>(ori.get()),sizeof(float)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(x.data()),sizeof(short)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(y.data()),sizeof(short)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(ori.data()),sizeof(float)*nrof_minutiae);
         is.read(reinterpret_cast<char*>(&des_len),sizeof(short));
 
-        is.read(reinterpret_cast<char*>(des.get()),sizeof(float)*nrof_minutiae*des_len);
+        is.read(reinterpret_cast<char*>(des.data()),sizeof(float)*nrof_minutiae*des_len);
 
-        MinutiaeTemplate minu_template(nrof_minutiae,x.get(),y.get(),ori.get(),des_len,des.get(),blkH, blkW, oimg.get());
+        MinutiaeTemplate minu_template(nrof_minutiae,x,y,ori,des_len,des,blkH, blkW);
         fp_template.add_template(minu_template);
     }
 
@@ -1205,17 +1209,18 @@ void Matcher::load_FP_template(const std::vector<uint8_t> &buf, RolledFPTemplate
                 "Number of Minutiae: " + std::to_string(nrof_minutiae) + ">" +
                 std::to_string(Max_Nrof_Minutiae)};
         }
-        is.read(reinterpret_cast<char*>(x.get()),sizeof(short)*nrof_minutiae);
-        is.read(reinterpret_cast<char*>(y.get()),sizeof(short)*nrof_minutiae);
-        is.read(reinterpret_cast<char*>(ori.get()),sizeof(float)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(x.data()),sizeof(short)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(y.data()),sizeof(short)*nrof_minutiae);
+        is.read(reinterpret_cast<char*>(ori.data()),sizeof(float)*nrof_minutiae);
         is.read(reinterpret_cast<char*>(&des_len),sizeof(short));
-        is.read(reinterpret_cast<char*>(des.get()),sizeof(float)*nrof_minutiae*des_len);
+        is.read(reinterpret_cast<char*>(des.data()),sizeof(float)*nrof_minutiae*des_len);
 
-        RolledTextureTemplatePQ texture_template(nrof_minutiae,x.get(),y.get(),ori.get(),des_len,des.get());
+        RolledTextureTemplatePQ texture_template(nrof_minutiae,x,y,ori,des_len,des);
         fp_template.add_texture_template(texture_template);
     }
 }
 
+#if 0
 int Matcher::load_single_template(string tname, TextureTemplate& texture_template)
 {
     ifstream is;
@@ -1321,6 +1326,7 @@ int Matcher::load_single_PQ_template(string tname, RolledTextureTemplatePQ& minu
     cout<<tname<<endl;
     return 0;
 }
+#endif
 
 vector<tuple<float, int, int>>  Matcher::LSS_R_Fast2_Dist(vector<tuple<float, int, int>> &corr, const SingleTemplate & latent_template, const SingleTemplate & rolled_template, float d_thr)
 const

matching/matcher.h
@@ -36,19 +36,23 @@ class Matcher final {
 
 public:
     Matcher(string code_file);
+#if 0
     int List2List_matching(string latent_list_file, string rolled_list_file, string output_file);
     int One2List_matching(string latent_template_file, string rolled_list_file, string score_file);
+#endif
     float One2One_minutiae_matching(const MinutiaeTemplate  &latent_minu_template, const MinutiaeTemplate  &rolled_minu_template, bool save_corr = false, string corr_file = "") const;
     float One2One_texture_matching(const LatentTextureTemplate &latent_texture_template, const RolledTextureTemplatePQ &rolled_minu_template) const;
     int One2One_matching_selected_templates(const LatentFPTemplate &latent_template, const RolledFPTemplate &rolled_template, vector<float> & score, bool save_corr = false, string corr_file = "") const;
     int One2One_matching_all_templates(const LatentFPTemplate &latent_template, const RolledFPTemplate &rolled_template, vector<float> & score);
+#if 0
     int One2One_matching(string latent_file, string rolled_file);
     int load_single_template(string tname, TextureTemplate& texture_template);
     int load_FP_template(string tname, LatentFPTemplate & fp_template);
-    void load_FP_template(const std::vector<uint8_t> &buf, LatentFPTemplate & fp_template) const;
     int load_FP_template(string tname, RolledFPTemplate & fp_template);
-    void load_FP_template(const std::vector<uint8_t> &buf, RolledFPTemplate & fp_template) const;
     int load_single_PQ_template(string tname, RolledTextureTemplatePQ& minu_template);
+#endif
+    void load_FP_template(const std::vector<uint8_t> &buf, LatentFPTemplate & fp_template) const;
+    void load_FP_template(const std::vector<uint8_t> &buf, RolledFPTemplate & fp_template) const;
 
 private:
     vector<tuple<float, int, int>>  LSS_R_Fast2_Dist(vector<tuple<float, int, int>> &corr, const SingleTemplate & latent_template,const SingleTemplate & rolled_template, float d_thr=30.0) const;

commit b3ebdaa83692e0a38c0d4447d11473536270ace8

Restore old likely buggy code.
Necessary to match output. Regression since b2775eb09b27260378d350aef35861584a505774

matching/include.h
@@ -15,6 +15,7 @@
 #define INCLUDE_H
 
 #include <cmath>
+#include <cstring>
 #include <numeric>
 #include <vector>
 
@@ -203,6 +204,16 @@ class RolledTextureTemplatePQ:public TextureTemplate
         std::vector<unsigned char> m_desPQ{};
         RolledTextureTemplatePQ() : TextureTemplate() {}
 
+	/*
+	 * FIXME: This hides des_length() from SingleTemplate, but is necessary.
+	 *        Make this a virtual hierarchy?
+	 */
+	int des_length() const  {
+		if (m_desPQ.size() == 0)
+			return (0);
+		return (m_desPQ.size() / m_minutiae.size());
+	}
+
         RolledTextureTemplatePQ(const int nrof_minutiae, const int des_length):TextureTemplate(nrof_minutiae, des_length),
             m_desPQ(nrof_minutiae*des_length)
         {
@@ -211,9 +222,18 @@ class RolledTextureTemplatePQ:public TextureTemplate
         RolledTextureTemplatePQ(const int nrof_minutiae, const std::vector<short> &x,const std::vector<short> &y,const std::vector<float> &ori, const std::vector<float> &des):
         TextureTemplate(nrof_minutiae, x, y, ori, {})
         {
-        	/* FIXME: m_desPQ is unsigned char, but storing floats? Bug? */
-        	for (const float &f : des)
-        		m_desPQ.emplace_back(static_cast<unsigned char>(f));
+        	/*
+        	 * The des vector is already sized with number of minutiae in
+        	 * mind, so we don't need to multiply by it.
+        	 */
+
+         	/*
+         	 * FIXME: m_desPQ is unsigned char, but storing floats? Bug?
+         	 *        Note above we can't pass it to the TextureTemplate
+         	 *        constructor and so it gets duplicated here.
+         	 */
+         	m_desPQ = std::vector<unsigned char>(des.size());
+         	std::memcpy(m_desPQ.data(), des.data(), sizeof(unsigned char)*des.size());
         }
 };
 